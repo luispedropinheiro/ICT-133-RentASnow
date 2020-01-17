da1d8b07b340c229b5e205fc4f952ebc3a9d24d7
FERNANDES-PINHEIRO Luis-Pedro <Luis-Pedro.FERNANDES-PINHEIRO@cpnv.ch>
2020-01-17 15:07:46 +0100

    SnowboardsDone
    Progress on the Projet

--- view/Snowboards.php
+++ view/Snowboards.php
@@ -8,29 +8,35 @@
 ob_start();
 ?>
     <h1></h1>
-    <table class="table table-bordered">
+    <table class="table table-bordered" style="text-align: center">
         <tr>
             <th>Modèle</th>
             <th>Marque</th>
-            <th>BigImage</th>
-            <th>SmallImage</th>
+            <th>Image</th>
             <th>DateRetour</th>
             <th>Disponible</th>
         </tr>
         <tr>
             <?php
             foreach ($snows as $snow)
-
-
+            {
+                echo "<tr><td>".$snow['modele']."</td><td>".$snow["marque"]."</td><td><img src='view/images/".$snow["smallimage"]."' width='500px'/></td><td>".$snow["dateretour"]."</td><td>"?>
+                <?php
+                    if($snow["disponible"] == 1)
+                    {
+                        echo "Disponible";
+                    }else
+                        {
+                            echo "Indisponible";
+                        }
+                    echo "</td></tr>";
+            }
             ?>
+
         </tr>
-
-
 
     </table>
 
-
-?>
 <?php
 $content = ob_get_clean();
 require_once ("gabarit.php");
